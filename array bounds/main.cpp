3e3d19ab3256602f5faea072cb165229db260845
20141105072 <2196690899@qq.com>
2015-12-09 18:28:51 +0800

    array bounds
    在程序中，定义两个数组，其中一个数组比另外一个数组短，内存在给两个数组分配存储空间的时候，就有可能造成空间上的不合法利用，会覆盖已经使用的内存空
    间。

--- array bounds/main.cpp
+++ array bounds/main.cpp
@@ -10,6 +10,17 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[3];
+    int b[5];
+    a[0]=10;
+    a[1]=15;
+    a[2]=20;
+    b[0]=2;
+    b[1]=4;
+    b[2]=6;
+    b[3]=8;
+    b[4]=10;
+    printf("a[0]=%d b[0]=%d a[1]=%d b[1]=%d a[2]=%d b[2]=%d", a[0],b[0],a[1],b[1],a[2],b[2]);
+    printf("&a[0]=%x &a[1]=%x &a[2]=%x &b[0]=%x &b[1]=%x &b[2]=%x &b[3]=%x &b[4]=%x\n",&a[0],&a[1],&a[2],&b[0],&b[1],&b[2]);
     return 0;
 }
